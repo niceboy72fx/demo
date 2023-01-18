79851fa0e0225499fcec36e8e8bb0020475fb8f7
darkside72fx <hoangtuathat@gmail.com>
2023-01-18 12:50:08 +0700

    update v2

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import Slider from "./component/slides";
 
 function App() {
   const [changeComponent, setChangeComponent] = useState(<Slider />);
+  const [checked, setChecked] = useState("checked");
+  const [checked2, setChecked2] = useState("");
   const change = (e) => {
     if (e == 1) {
-      setChangeComponent(<Gallery />);
+      setChangeComponent(<Gallery className="ease-out duration-300" />);
+      setChecked("");
+      setChecked2("checked");
     } else if (e == 0) {
-      setChangeComponent(<Slider />);
+      setChangeComponent(<Slider className="ease-out duration-300" />);
+      setChecked2("");
+      setChecked("checked");
     }
     console.log(e);
   };
   return (
     <div className="App flex flex-col justify-center items-center">
       {changeComponent}
-      <div className="m-4">
+      <div className="m-2">
         <input
           type="radio"
           name="slider"
           value="0"
-          checked="checked"
+          checked={checked}
           onChange={(e) => change(e.target.value)}
           className="m-3"
         />
@@ -29,6 +35,7 @@ function App() {
           type="radio"
           name="slider"
           value="1"
+          checked={checked2}
           onChange={(e) => change(e.target.value)}
           className="m-3"
         />
